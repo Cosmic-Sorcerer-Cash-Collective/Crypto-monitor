baafcceb89926da31ccd2b0d8b1c97823212ec7d
mistercouscous <erwan-baillon@orange.fr>
2024-04-18 22:25:25 +0200

    UPDATE configuration ngnix

--- deploy/database/database.sql
+++ deploy/database/database.sql
@@ -1,16 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  user_id varchar(255) NOT NULL,
-  created_at timestamp without time zone NOT NULL,
-  updated_at timestamp without time zone NOT NULL,
-  UNIQUE (user_id)
-);
-
-CREATE TABLE IF NOT EXISTS wallets (
-  id SERIAL PRIMARY KEY,
-  user_id varchar(255) NOT NULL,
-  balance decimal(10,2) NOT NULL,
-  created_at timestamp without time zone NOT NULL,
-  updated_at timestamp without time zone NOT NULL,
-  UNIQUE (user_id)
+CREATE TABLE IF NOT EXISTS favoritePair (
+    id  SERIAL PRIMARY KEY,
+    pair text NOT NULL
 );
