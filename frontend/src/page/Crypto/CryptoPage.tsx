a9a895f93b671d4f60e1a4a720a30a705cbb4425
mistercouscous <erwan-baillon@orange.fr>
2024-05-04 22:30:52 +0200

    ADD grid on pages bot and crypto

--- frontend/src/page/Crypto/CryptoPage.tsx
+++ frontend/src/page/Crypto/CryptoPage.tsx
@@ -1,12 +1,28 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
 import { MainLayout } from "../../components/layout";
+import SearchBar from "../../components/SearchBar";
 
 export function CryptoPage() {
   return (
     <MainLayout>
-      <Heading as="h1" size="md" textAlign="center">
-        Crypto
-      </Heading>
+      <Box minW="80vw" maxW="100%" m="0 auto" height="100%" minH="calc(90vh - 100px)" border="1px solid #e2e8f0" borderRadius="md" boxShadow="md" p={4}>
+      <Grid templateAreas={`"filter searchBar"
+                            "filter content"`}
+                            gap={1}
+                            gridTemplateColumns={"250px 1fr"}
+                            gridTemplateRows={"auto 1fr"}
+                           height="100%">
+        <GridItem gridArea="searchBar">
+          <SearchBar placeholder="Rechercher un bot" onSearch={(term) => console.log(term)} />
+        </GridItem>
+        <GridItem gridArea="filter">
+          <Heading>Crypto</Heading>
+        </GridItem>
+        <GridItem gridArea="content">
+          <Heading>Contenu</Heading>
+        </GridItem>
+      </Grid>
+      </Box>
     </MainLayout>
   );
 }
