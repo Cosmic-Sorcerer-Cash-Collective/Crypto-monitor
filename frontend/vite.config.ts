4e559481e7a63a224abff55be1d0a7edfd5a7c38
mistercouscous <erwan-baillon@orange.fr>
2024-05-07 16:32:01 +0200

    UPDATE config vitejs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: true,
-    port: 5173
-  }
+    strictPort: true,
+    port: 5173,
+  },
 })
